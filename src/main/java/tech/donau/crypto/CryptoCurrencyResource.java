a506fd9c6b89eb75448ef2fc28cf902c2cd445e1
MRasool <rasoolavazeh@yahoo.com>
2020-12-01 11:14:43 +0330

    Using files in quarkus, using rest client with multipart

--- src/main/java/tech/donau/crypto/CryptoCurrencyResource.java
+++ src/main/java/tech/donau/crypto/CryptoCurrencyResource.java
@@ -1,27 +1,41 @@
 package tech.donau.crypto;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import tech.donau.crypto.data.Currency;
+import tech.donau.crypto.data.MultipartBody;
 import tech.donau.crypto.service.CurrencyService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
 import java.util.Collection;
 
-@Path("/crypto")
+@Path("/echo")
 public class CryptoCurrencyResource {
 
     @Inject
     @RestClient
     CurrencyService currencyService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Currency> hello(@QueryParam("id") String id) {
-        return currencyService.getCurrency(id);
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String echoFile(String body) {
+        return body;
     }
+
+
+    @POST
+    @Path("/test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String callEcho() {
+        final MultipartBody multipartBody = new MultipartBody();
+        multipartBody.setFile(new ByteArrayInputStream("Hello World".getBytes()));
+        multipartBody.setName("hello.txt");
+        return currencyService.sendFile(multipartBody);
+    }
+
 }
